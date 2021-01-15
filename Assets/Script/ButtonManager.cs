75920139df6a787447e1358149dc55d47024fd2a
nur <imecenu@mef.edu.tr>
2021-01-15 23:18:18 +0300

    Hello

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private Button btn;
+   
+    private int _itemId;
+    [SerializeField]  private Sprite _buttonTexture;
+    [SerializeField] private RawImage buttonImage;
+  
+
+    public int ItemId
+    {
+        set { _itemId = value; }
+      
+    }
+
+    public Sprite ButtonTexture
+    {
+        set { _buttonTexture = value;
+
+            buttonImage.texture = _buttonTexture.texture; 
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(SelectObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UIManager.Instance.OnEntered(gameObject))
+        {
+            transform.localScale = Vector3.one * 2;
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+        
+    }
+
+    void SelectObject()
+    {
+        DataHandler.Instance.SetFurniture(_itemId);
+      
+    }
+
+    
+}
+
+
---
+++ Assets/Script/inputManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class inputManager : MonoBehaviour
+{
+    [SerializeField] private Camera arCam;
+    [SerializeField] private ARRaycastManager _raycastManager;
+    private GameObject crosshair;
+
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    private Touch touch;
+    private Pose pose;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // get the components
+        _raycastManager = FindObjectOfType<ARRaycastManager>();
+        crosshair = transform.GetChild(0).gameObject;
+
+        // hide the placement indicator visual
+        crosshair.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), _hits, TrackableType.Planes);
+
+
+
+        touch = Input.GetTouch(0);
+
+        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began)
+            return;
+
+
+        if (IsPointerOverUI(touch)) return;
+
+        transform.position = _hits[0].pose.position;
+        transform.rotation = _hits[0].pose.rotation;
+
+        // enable the visual if it's disabled
+        if (!crosshair.activeInHierarchy)
+            crosshair.SetActive(true);
+
+        Instantiate(DataHandler.Instance.GetFurniture(), transform.position, transform.rotation);
+
+    }
+
+
+    bool IsPointerOverUI(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+
+
+
+}
